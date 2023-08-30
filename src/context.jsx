801b8606a063925e73d95a9dd96010ff03ac47a1
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2023-08-30 21:33:48 +0300

    add db

--- src/context.jsx
+++ src/context.jsx
@@ -2,9 +2,21 @@ import React, { useContext, useEffect } from 'react';
 
 const AppContext = React.createContext();
 
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a';
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
 export const AppProvider = ({ children }) => {
   useEffect(() => {
-    console.log('fatch data here');
+    const fetchData = async () => {
+      try {
+        const response = await fetch(allMealsUrl);
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
   }, []);
   return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
 };
