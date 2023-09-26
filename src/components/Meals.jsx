9867ea9ac26b94bd5ca442757d9c8951b804c49e
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2023-09-26 13:00:03 +0300

    add icon, loading

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,23 +1,29 @@
 import { useGlobalContext } from '../context';
+import { SlLike } from 'react-icons/sl';
 
 function Meals() {
-  const { meals } = useGlobalContext();
+  const { meals, loading } = useGlobalContext();
+
+  if (loading) {
+    return (
+      <section className="section">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
   return (
     <section className="section-center">
       {meals.map(meal => {
         const { idMeal, strMealThumb: image, strMeal: title } = meal;
         console.log(meal);
         return (
-          <article key={idMeal} className="single=meal">
-            <img
-              src={image}
-              alt={title}
-              className="img"
-              style={{ width: '200px' }}
-            />
+          <article key={idMeal} className="single-meal">
+            <img src={image} alt={title} className="img" />
             <footer>
               <h5>{title}</h5>
-              <button className="like-btn">click me</button>
+              <button className="like-btn">
+                <SlLike />
+              </button>
             </footer>
           </article>
         );
--- src/context.jsx
+++ src/context.jsx
@@ -4,25 +4,30 @@ import axios from 'axios';
 const AppContext = React.createContext();
 
 const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
-const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+// const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 export const AppProvider = ({ children }) => {
   const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchMeals = async url => {
+      setLoading(true);
       try {
         const { data } = await axios.get(url);
         setMeals(data.meals);
       } catch (error) {
         console.log(error.response);
       }
+      setLoading(false);
     };
     fetchMeals(allMealsUrl);
   }, []);
 
   return (
-    <AppContext.Provider value={{ meals }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ meals, loading }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
